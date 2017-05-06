0ea971e7834527c11ab937c818d724fc9062d1e4
amatokus8669 <amatokus8669@users.noreply.github.com>
2017-05-06 16:57:35 +0200

    Add files via upload

---
+++ 4.1/Main/ArretMoteur.ino
@@ -0,0 +1,18 @@
+void arretMoteur(String moteur){
+  if(moteur == "X"){
+      sensX = "";
+    digitalWrite(pinXrun, LOW);
+    digitalWrite(pinXH,LOW);
+    digitalWrite(pinXAH,LOW);
+  }else if(moteur == "Y"){
+      sensY = "";
+    digitalWrite(pinYrun, LOW);
+    digitalWrite(pinYH,LOW);
+    digitalWrite(pinYAH,LOW);
+  }else if(moteur == "Z"){
+      sensZ = "";
+    digitalWrite(pinZrun, LOW);
+    digitalWrite(pinZH,LOW);
+    digitalWrite(pinZAH,LOW);
+  }
+}
---
+++ 4.1/Main/MotorControl.ino
@@ -0,0 +1,42 @@
+void groupedMovement(float desiredX, float desiredY, float desiredZ){
+  float desiredPosition[] = {desiredX, desiredY, desiredZ};
+  float actualPosition[] = {nacellePositionX, BrasAngleY, NacelleHauteurZ};
+  while(array_comp(desiredPosition,actualPosition) == false){
+    Serial.print(desiredPosition[0]);
+    Serial.print(desiredPosition[1]);
+    Serial.println(desiredPosition[2]);
+    Serial.print(actualPosition[0]);
+    Serial.print(actualPosition[1]);
+    Serial.println(actualPosition[2]);
+    getMotorData(X);
+    getMotorData(Y);
+    getMotorData(Z);
+    if(desiredX != nacellePositionX){
+      if(desiredX>nacellePositionX){
+        mouvementMoteur(X,H,100);
+      }else if(desiredX<nacellePositionX){
+        mouvementMoteur(X,AH,100);
+      }
+      }else{arretMoteur(X);}
+    if(desiredY != BrasAngleY){
+      if(desiredY>BrasAngleY){
+        mouvementMoteur(Y,H,100);
+      }else if(desiredY<BrasAngleY){
+        mouvementMoteur(Y,AH,100);
+      }
+      }else{arretMoteur(Y);}
+    if(desiredZ != NacelleHauteurZ){
+      if(desiredZ>NacelleHauteurZ){
+        mouvementMoteur(Z,H,100);
+      }else if(desiredZ<NacelleHauteurZ){
+        mouvementMoteur(Z,AH,100);
+      }
+      }else{arretMoteur(Z);}
+      actualPosition[0] = nacellePositionX;
+      actualPosition[1] = BrasAngleY;    
+      actualPosition[2] = NacelleHauteurZ;        
+  }
+}
+
+
+

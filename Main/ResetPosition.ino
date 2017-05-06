3d08263aa3f1364f000c1d3509893c9eae3457c7
amatokus8669 <amatokus8669@users.noreply.github.com>
2017-05-06 16:30:08 +0200

    Add files via upload

---
+++ Main/ResetPosition.ino
@@ -0,0 +1,15 @@
+void posReset(String Motor){
+  if(Motor == X){
+    if(digitalRead(pinMotorXSwitch) == LOW){
+    nacellePositionX = 0;
+   } 
+  }else if(Motor == Y){
+    if(digitalRead(pinMotorYSwitch) == LOW){
+    BrasAngleY = 0;
+   } 
+  }else if(Motor == Z){
+    if(digitalRead(pinMotorZSwitch) == LOW){
+    NacelleHauteurZ = 0;
+   } 
+  }
+}
